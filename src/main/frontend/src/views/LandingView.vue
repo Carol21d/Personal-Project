bed3fbaa701345e55759ea6edcb5ddcddf1f6d35
blake S <design.harls@icloud.com>
2023-03-18 19:31:09 +0100

    maquetacion de landing estructura

---
+++ src/main/frontend/src/views/LandingView.vue
@@ -0,0 +1,14 @@
+<script></script>
+<template>
+  <main class="wrapper">
+
+      <p  class="wrapper__title">We are going to explore!!!</p>
+      <img src="../assets/images/background--img.png" alt="background--image" />
+
+      <div class="d-flex justify-space-between">
+        <v-btn variant="tonal">Enter</v-btn>
+      </div>
+    
+  </main>
+</template>
+<style lang="scss" scoped></style>
