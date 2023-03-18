df6bcef9f7523030a7bda637530acecbefe64ed0
blake S <design.harls@icloud.com>
2023-03-18 18:05:09 +0100

    init project

---
+++ src/test/java/ProjectPersonal/example/ProjectPersonal/ProjectPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectPersonal.example.ProjectPersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
