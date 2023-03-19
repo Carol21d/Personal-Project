56e3be4199d877c513a35b89999f196e62628ed9
blake S <design.harls@icloud.com>
2023-03-19 01:15:42 +0100

    nav

--- src/main/frontend/src/components/Navbar.vue
+++ src/main/frontend/src/components/Navbar.vue
@@ -1,21 +1,20 @@
 <script>
- export default {
-    data () {
-      return {
-        drawer: true,
-        items: [
-          { title: 'Home', icon: 'mdi-home-city' },
-          { title: 'Search', icon: 'mdi-account' },
-          { title: 'Categories', icon: 'mdi-account-group-outline' },
-        ],
-        rail: true,
-      }
-    },
-  
- }
+export default {
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: "Search", icon: "mdi-magnify" },
+        { title: "Home", icon: "mdi-home" },
+        { title: "Categories", icon: "mdi-view-dashboard" },
+      ],
+      rail: true,
+    };
+  },
+};
 </script>
 <template>
-<v-card>
+  <v-card>
     <v-layout>
       <v-navigation-drawer
         v-model="drawer"
@@ -40,15 +39,25 @@
         <v-divider></v-divider>
 
         <v-list density="compact" nav>
-            <v-list-item prepend-icon="mdi-account" title="Home" value="home"></v-list-item>
-            <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-group-outline" title="Categories" value="categories"></v-list-item>
+          <v-list-item
+            prepend-icon="mdi-magnify"
+            title="Search"
+            value="search"
+          ></v-list-item>
+          <v-list-item
+            prepend-icon="mdi-home"
+            title="Home"
+            value="home"
+          ></v-list-item>
+          <v-list-item
+            prepend-icon="mdi-view-dashboard"
+            title="Categories"
+            value="categories"
+          ></v-list-item>
         </v-list>
       </v-navigation-drawer>
-      <v-main style="height: 250px"></v-main>
+      <v-main style="height: 95px"><h1>Hola, Ruteros !!!</h1></v-main>
     </v-layout>
   </v-card>
-   
 </template>
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
