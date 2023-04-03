d3c4d8199c7cccad78b4a3fff60621557b2c8a30
blake S <design.harls@icloud.com>
2023-04-03 23:59:57 +0200

    formulario para comentarios

--- src/main/frontend/src/components/Comentario.vue
+++ src/main/frontend/src/components/Comentario.vue
@@ -1,66 +1,50 @@
 <script>
-  export default {
+export default {
     data: () => ({
-      dialog: false,
+        dialog: false,
     }),
-  }
+}
 </script>
 
-    <template>
-        <v-row justify="center mt-12">
-            <v-dialog v-model="dialog" persistent width="1024">
-                <template v-slot:activator="{ props }">
-                    <v-btn color="primary" v-bind="props">
-                        AÑADIR COMENTARIO
+<template>
+    <v-row justify="center mt-12">
+        <v-dialog v-model="dialog" persistent width="600px">
+            <template v-slot:activator="{ props }">
+                <v-btn color="primary" v-bind="props">
+                    AÑADIR COMENTARIO
+                </v-btn>
+            </template>
+            <v-card>
+                <v-card-title>
+                    <span class="text-h5">FORMULARIO DE COMENTARIOS</span>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field label="Name*" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-textarea v-model="comentario" auto-grow variant="filled" color="deep-purple"
+                                    label="Comentario*" rows="1"></v-textarea>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                    <small>*indicates required field</small>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
+                        Close
                     </v-btn>
-                </template>
-                <v-card>
-                    <v-card-title>
-                        <span class="text-h5">FORMULARIO DE COMENTARIOS</span>
-                    </v-card-title>
-                    <v-card-text>
-                        <v-container>
-                            <v-row>
-                                <v-col cols="12" sm="6" md="4">
-                                    <v-text-field label="Legal first name*" required></v-text-field>
-                                </v-col>
-                                <v-col cols="12" sm="6" md="4">
-                                    <v-text-field label="Legal middle name"
-                                        hint="example of helper text only on focus"></v-text-field>
-                                </v-col>
-                                <v-col cols="12" sm="6" md="4">
-                                    <v-text-field label="Legal last name*" hint="example of persistent helper text"
-                                        persistent-hint required></v-text-field>
-                                </v-col>
-                                <v-col cols="12">
-                                    <v-text-field label="Email*" required></v-text-field>
-                                </v-col>
-                                <v-col cols="12">
-                                    <v-text-field label="Password*" type="password" required></v-text-field>
-                                </v-col>
-                                <v-col cols="12" sm="6">
-                                    <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*" required></v-select>
-                                </v-col>
-                                <v-col cols="12" sm="6">
-                                    <v-autocomplete
-                                        :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                                        label="Interests" multiple></v-autocomplete>
-                                </v-col>
-                            </v-row>
-                        </v-container>
-                        <small>*indicates required field</small>
-                    </v-card-text>
-                    <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
-                            Close
-                        </v-btn>
-                        <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
-                            Save
-                        </v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-dialog>
-        </v-row>
-    </template>
+                    <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
+                        Save
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-row>
+</template>
+
+
 
