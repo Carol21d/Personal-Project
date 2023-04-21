ed924e9abe2c36ca3783752817937b3c90a7f932
blake S <design.harls@icloud.com>
2023-04-21 22:33:16 +0200

    configurando routes

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -6,21 +6,7 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <!-- <Dashboardview /> -->
-  <!-- <AboutView/> -->
 
-  <!-- <header>
-  
-
-     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div> -->
-  <!-- </header> -->
 
   <RouterView />
 </template>
--- src/main/frontend/src/router/index.js
+++ src/main/frontend/src/router/index.js
@@ -11,11 +11,41 @@ const router = createRouter({
       component: LandingView
     },
     {
-      path: '/dashboard',
-      name: 'Dashboard',
-      // component: () => import('../views/DashboardView.vue')
-      component: Dashboard
+      path: '/dashboard/id',
+      name: 'dashboard',
+      component:() => import('@/views/DashboardView.vue')
+    },
+    {
+      path: '/aquatic',
+      name: 'aquatic',
+      component: () =>import('@/views/AquaticView.vue')
+    },
+    {
+      path: '/bikes',
+      name: 'bike',
+      component: () =>import('@/views/BikesView.vue')
+    },
+    {
+      path: '/climbing',
+      name: 'climbing',
+      component: () =>import('@/views/ClimbingView.vue')
+    },
+    {
+      path: '/combined',
+      name: 'combined',
+      component: () =>import('@/views/CombinedView.vue')
+    },
+    {
+      path: '/mountains',
+      name: 'mountains',
+      component: () =>import('@/views/MountainsView.vue')
+    },
+    {
+      path: '/pets',
+      name: 'pets',
+      component: () =>import('@/views/PetsView.vue')
     }
+
   ]
 })
 
