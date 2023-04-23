16c3cd715379138e71b2e3bdb6c093b8da96781a
blake S <design.harls@icloud.com>
2023-04-23 20:53:21 +0200

    metodos crud

--- src/main/frontend/src/stores/commentsUser.js
+++ src/main/frontend/src/stores/commentsUser.js
@@ -20,7 +20,7 @@ export const commentsStore = defineStore("commentsStore",{
 
         async editComment(comment, id){
             try{
-                const response = await axios.put("http://localhost:8080/api/comments", comment);
+                const response = await axios.put("http://localhost:8080/api/comments/${id}", comment);
                 console.log(response.status);
             }catch(error){
                 console.error(error);
@@ -30,9 +30,22 @@ export const commentsStore = defineStore("commentsStore",{
 
         async deleteComments(id) {
             try{
-                const response = await axios.de
+                const response = await axios.delete("http://localhost:8080/api/comments/${id}");
+                console.log(response.status);
+                return response.status;
+            }catch(error){
+                console.error(error);
             }
 
-        }
-    }
-})
+        },
+
+        async fetchComments(){
+            try{
+                const response = await axios.get("http://localhost:8080/api/comments");
+                this.comments = response.data;
+            }catch(error){
+                console.error(error);
+            }
+        },
+    },
+});
