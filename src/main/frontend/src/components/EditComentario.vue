08392619f044c1fd56c49bd37e9fb4e04e608d74
blake S <design.harls@icloud.com>
2023-04-24 21:53:14 +0200

    comments finished

--- src/main/frontend/src/components/EditComentario.vue
+++ src/main/frontend/src/components/EditComentario.vue
@@ -1,22 +1,22 @@
 <script setup>
 import { commentsStore } from '../stores/commentsUser';
-import {ref ,defineProps} from 'vue';
+import { ref, defineProps } from 'vue';
 
 const props = defineProps({
     id: {
-    type: Number,
-    required:true,
-  },
+        type: Number,
+        required: true,
+    },
 
-  comment:{
-    type: String,
-    required:true,
-  },
+    comment: {
+        type: String,
+        required: true,
+    },
 
-  name:{
-    type:String,
-    required:true,
-  },
+    name: {
+        type: String,
+        required: true,
+    },
 });
 
 
@@ -26,26 +26,26 @@ const commentstore = commentsStore();
 
 let editActivada = ref(false);
 
-function editComment(){
-    const updateComments ={
-        id:props.id,
+function editComment() {
+    const updateComments = {
+        id: props.id,
         name: name.value,
         comment: comment.value,
     };
-     commentstore.editComment(updateComments,updateComments.id);
-     editActivada.value = false;
+    commentstore.editComment(updateComments, updateComments.id);
+    editActivada.value = false;
 };
 
-function updateEdit (){
-     
+function updateEdit() {
 
-   (editActivada.value == false) ? editActivada.value = true : editActivada.value = false;
-   console.log(editActivada.value);
+
+    (editActivada.value == false) ? editActivada.value = true : editActivada.value = false;
+    console.log(editActivada.value);
 }
 
 
-function deleteComment(){
-      commentstore.deleteComments(props.id)
+function deleteComment() {
+    commentstore.deleteComments(props.id)
 }
 
 
@@ -56,74 +56,105 @@ function deleteComment(){
             <v-img contain aspect-ratio="16/9" src="../src/assets/images/avatar2.png" alt="avatar-user"></v-img>
         </v-avatar>
         <div class="btn">
-            <v-btn variant="tonal" class="submit" color="light-green darken-4" v-on:click="updateEdit()">EDIT</v-btn>
-            <v-btn variant="tonal" class="submit" color="light-green darken-4" v-if="editActivada== true" v-on:click="editComment()">CONFIRMAR</v-btn>
-            <v-btn variant="tonal" class="mr-5"  color="red accent-4 " v-on:click="deleteComment()">DELETE</v-btn>
+            <v-btn variant="tonal" class="btn-edit" color="light-green darken-4" v-on:click="updateEdit()">EDIT</v-btn>
+            <v-btn variant="tonal" class="btn-confirmar" color="light-green darken-4" v-if="editActivada == true"
+                v-on:click="editComment()">CONFIRMAR</v-btn>
+            <v-btn variant="tonal"  class="btn-cancel mr-5" color="red accent-4 " v-on:click="deleteComment()">DELETE</v-btn>
         </div>
         <div class="comment">
             <v-card-text v-if="editActivada === false" class="nameU">User: {{ props.name }}</v-card-text>
-             <input placeholder="Name: " type="text" v-if="editActivada === true" v-model="name">
+            <input placeholder="Name: " type="text" v-if="editActivada === true" v-model="name">
             <v-card-text v-if="editActivada === false" class="textU">Comentario: {{ props.comment }}</v-card-text>
             <input type="text" v-if="editActivada === true" v-model="comment" placeholder="Comment">
 
         </div>
- 
+
     </div>
-    </template>
-    <style lang="scss" scoped>
-    .wrapper {
-        display: flex;
-        // position: relative;
-        // left: 52.7%;
-        // top: 50%;
-        background-color: whitesmoke;
-        width: 43%;
-        height: 25%;
-        border-radius: 10px;
-        border: 2px solid black;
-        .avatar {
-            bottom: 25%;
-            right: 1%;
-        }
-        .btn {
-            display: flex;
-            position: absolute;
-            justify-content: flex-end;
-            gap: 7%;
-            top: 78%;
-            left: 64%;
-    
-        }
+</template>
+<style lang="scss" scoped>
+.wrapper {
+    //     display: flex;
+    //     position: relative;
+    //     flex-direction: row;
+    //     left: 52.7%;
+    //    top: 120%;
 
-        .comment{
-            display: flex;
-            flex-direction: column;
-            justify-content: start;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    top: 43vh;
+    align-items: center;
+    margin-bottom: 10px;
+    background-color: whitesmoke;
+    width: 43%;
+    height: 25%;
+    border-radius: 10px;
+    border: 2px solid black;
 
-            .nameU{
-                display: flex;
-                position: relative;
-                bottom: 5%;
-                color: firebrick;
-                font-weight: 600;
-                font-size: large;
-            }
-            .textU{
-                font-size: large;
-                display: flex;
-                position: relative;
-                bottom: 42%;
-                right: 12%;
-                font-family: Verdana, Geneva, Tahoma, sans-serif;
-
-            }
-              
-              
-
-
-        }
+    .avatar {
+        bottom: 5vh;
+        right: 1%;
     }
-    </style>
+
+    .btn {
+        display: flex;
+        position: absolute;
+        justify-content: flex-end;
+        gap: 7%;
+        top: 78%;
+        left: 64%;
+
+        .btn-edit{
+            display: flex;
+            position: relative;
+            bottom: 2vh;
+        }
+        .btn-cancel{
+            display: flex;
+            position: relative;
+            bottom: 2vh;
+        }
+
+        .btn-confirmar{
+            display: flex;
+            position: relative;
+            bottom: 2vh;
+        }
+
+    }
+
+    .comment {
+        display: flex;
+        flex-direction: column;
+        justify-content: start;
+        position: relative;
+        bottom:82%;
+
+        .nameU {
+            display: flex;
+            position: relative;
+            bottom: 1vh;
+            color: firebrick;
+            font-weight: 600;
+            font-size: large;
+        }
+
+        .textU {
+            font-size: large;
+            display: flex;
+            position: relative;
+            bottom: 4vh;
+            right: 12%;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+
+        }
+
+
+
+
+    }
+}
+</style>
     
     
 
--- src/main/frontend/src/views/AboutView.vue
+++ src/main/frontend/src/views/AboutView.vue
@@ -31,44 +31,76 @@ onBeforeMount(async() => {
 
 </script>
 <template>
-  <div class="container">
-    <Greeting class="saludo d-flex"/>
-    <SocialNetwork/>
+  <div class="wrapper">
+    <div class="container">
+      <Greeting class="saludo d-flex"/>
+      <SocialNetwork/>
+    </div>
+    <div>
+      <v-btn class="btn-back" prepend-icon="mdi-arrow-left" variant="tonal" v-on:click="goback()">Back
+      </v-btn>
+    </div>
+    <v-card-title class="comentarios d-flex text-h6 text-md-h5 font-italic text-lg-h4">Comments</v-card-title>
+    <Comentario /> 
+    <div class="container-edit">
+      <EditComentario class="editar" v-for="comments of commentstore.comments" :id="comments.id"   :name="comments.name" :comment="comments.comment"/>
+    </div>
+    
+    
+    <CardDetails class="card-detail"/>
+    
+    <Navbar />
   </div>
-  <div>
-    <v-btn class="btn-back" prepend-icon="mdi-arrow-left" variant="tonal" v-on:click="goback()">Back
-    </v-btn>
-  </div>
-  <v-card-title class="comentarios d-flex text-h6 text-md-h5 font-italic text-lg-h4">Comments</v-card-title>
-  <CardDetails class="card-detail"/>
-  <Comentario /> 
-  <EditComentario v-for="comments of commentstore.comments" :id="comments.id"   :name="comments.name" :comment="comments.comment"/>
-  <Navbar />
 </template>
-
-
-
-
-
 <style lang="scss" scoped>
- .btn-back{
-  display: flex;
-  position: absolute;
-  left: 10%;
-  top: 12%;
+.wrapper{
+  .btn-back{
+   display: flex;
+   position: absolute;
+   left: 10%;
+   top: 12%;
+  }
+
+ 
+ .comentarios{
+   position:absolute;
+   left: 53%;
+   top: 15%;
  }
-.comentarios{
-  position:absolute;
-  left: 53%;
-  top: 15%;
-}
-.card-detail{
+ .card-detail{
+   display: flex;
+   position:absolute;
+   right: 47%;
+   bottom: 24%;
+ }
+ .container-edit{
   display: flex;
-  position:absolute;
-  right: 47%;
-  bottom: 24%;
+    flex-direction: column;
+    align-items: flex-end;
+    margin-bottom: 10px; 
+   
+   .editar{
+       display: flex;
+       flex-direction: row;
+       align-items: center;
+       margin-bottom: 10px; 
+       position: relative;
+       right: 3.4%;
+      
+   
+   }
+
+ }
+  
 }
 </style>
+
+
+
+
+
+
+
  
 
   
